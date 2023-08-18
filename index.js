56119a9351424e74e16dfa84053f7255073e2d0b
tuxoak <dangerdasedicoes@gmail.com>
2023-08-18 17:43:08 -0300

    error path

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
+function cleaningError(error){
+    console.log(error);
+    throw new Error(chalk.red(error.code, 'não há arquivo no diretório..'));
+}
+
 function searchArchive(pathing){
     const encoding = 'utf-8';
-    fs.readFile(pathing, encoding, (_, text) =>{
+    fs.readFile(pathing, encoding, (error, text) =>{
+        if(error){
+            cleaningError(error);
+        }
         console.log(chalk.blue(text));
     })
 }
