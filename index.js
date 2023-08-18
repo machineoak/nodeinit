c9be4d2c02900f1b08c5916443af9a231103f1f7
tuxoak <dangerdasedicoes@gmail.com>
2023-08-18 17:30:14 -0300

    lib fs

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+import fs from 'fs';
 import chalk from 'chalk';
 
-console.log(chalk.black('test'));
+function searchArchive(pathing){
+    const encoding = 'utf-8';
+    fs.readFile(pathing, encoding, (_, text) =>{
+        console.log(chalk.blue(text));
+    })
+}
+
+searchArchive('./archive/README.md');
