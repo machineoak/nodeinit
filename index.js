bdd23620d12efacd8e9bb765d07461612c9aafb5
keven carvalho <dangerdasedicoes@gmail.com>
2023-08-27 21:33:34 -0300

    ending

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
+function extraiLinks(text) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const capturas = [...texto.matchAll(regex)];
+    console.log(capturas)
+    const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
+    return resultados;
+}
+
 function cleaningError(error){
     console.log(error);
     throw new Error(chalk.red(error.code, 'não há arquivo no diretório..'));
@@ -11,15 +19,13 @@ function cleaningError(error){
 async function searchArchive(pathing){
     try {
         const encoding = 'utf-8';
-    const text = await fs.promises.readFile(pathing,
-    encoding)
-    console.log(chalk.green(text))
+        const text = await fs.promises.readFile(pathing, encoding)
+        console.log(extraiLinks(text));
     } catch (error){
         cleaningError(error);
     }
 }
 
 searchArchive('./archive/README.md');
-
 // \[[^[\]]*?\]
 // \(https?:\/\/[^\s?#.].[^\s]*\)
